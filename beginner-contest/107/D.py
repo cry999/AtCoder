5efec4fdeae8549be7bc41320bfdf82ae31ef97e
cry999 <when.the.cry999@gmail.com>
2019-03-08 13:55:45 +0900

    AtCoder Beginner Contest 107

--- beginner-contest/107/D.py
+++ beginner-contest/107/D.py
@@ -1,100 +1,38 @@
-class BIT:
-    """1-indexed BIT
-    """
-
-    def __init__(self, size: int):
-        """サイズを指定して BIT を初期化します。
-        """
-        # BIT のサイズは 2^n + 1
-        self.__size = 1
-        while self.__size < size:
-            self.__size *= 2
-        self.__size += 1
-
-        # 実際の配列
-        self.__bit = [0] * self.__size
-
-    def add(self, idx: int, val: int):
-        """位置 idx (1-indexed) に値 val をたす。
-        :param idx: 値を追加したいインデックス
-        :param val: 追加したい値
-        """
-        while idx < self.__size:
-            self.__bit[idx] += val
-            idx += idx & (-idx)
-
-    def sum(self, l: int, r: int)->int:
-        """(l, r] の累積和を計算する。
-        :param l: left index
-        :param r: right index
-        :return: (l, r] の累積和
-        """
-        return self.__sum__(r) - self.__sum__(l)
-
-    def __sum__(self, idx: int)->int:
-        s = 0
-        while idx > 0:
-            s += self.__bit[idx]
-            idx -= idx & (-idx)
-        return s
-
-
-def convert(A: list)->list:
-    B = sorted(A)
-    C = []
-    N = len(A)
-    for a in A:
-        l, r = 0, N
-        while 1 < r - l:
-            m = (l+r)//2
-            if a < B[m]:
-                r = m
+def median_of_medians(N: int, A: int)->int:
+    def check(x):
+        print('check({})'.format(x))
+        offset = N
+        S = 0
+        r = 0
+        y = 0
+        D = [0] * (2 * N + 1)
+        for i in range(N):
+            print('A[{}] = {} ? {}'.format(i, A[i], x))
+            D[S + offset] += 1
+            if A[i] < x:
+                r += D[S + offset]
+                S += 1
             else:
-                l = m
-        C.append(r)
-    return C
+                S -= 1
+                r -= D[S + offset]
+            print(D, r)
+            print(('   ' * (S + offset)) + ' ^')
+            y += r
+        return y
 
-
-def inversion(A: list)->int:
-    n = len(A)
-    _A = convert(A)
-    bit = BIT(n)
-    s = 0
-    for j, a in enumerate(_A):
-        s += j - bit.sum(0, a)
-        bit.add(a, 1)
-    return s
-
-
-def check(A: list, x: int)->int:
-    # n = len(A)
-    # bit = BIT(n)
-    # for i, a in enumerate(A):
-    #     bit.add(i+1, 1 if a >= x else -1)
-
-    s = 0
-    B = []
-    for a in A:
-        s += 1 if a >= x else -1
-        B.append(s)
-    return inversion(B)
-    # return inversion([bit.sum(0, i+1) for i in range(n)])
-
-
-def median_of_medians(N: int, A: list)->int:
     alpha = sorted(A)
     l, r = 0, N
-    m, c = N//2, N*(N+1)//2
+    c = N * (N + 1) // 2
     while True:
-        if check(A, alpha[m]) <= c//2:
-            if m == N-1:
+        m = (l + r) // 2
+        if check(alpha[m]) <= c // 2:
+            if m == N - 1:
                 break
-            elif check(A, alpha[m+1]) > c//2:
+            elif check(alpha[m + 1]) > c // 2:
                 break
-            else:
-                l, m = m, (m+r)//2
+            l = m
         else:
-            m, r = (m+l)//2, m+1
+            r = m + 1
 
     return alpha[m]
 
